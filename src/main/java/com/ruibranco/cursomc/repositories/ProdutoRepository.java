ed483e89a315ac834ab6bcdc5fd847dca0f0cbbc
Rjborg <rjb.inf@gmail.com>
2019-12-02 18:04:56 +0000

    Produto e associação muitos para muitos

---
+++ src/main/java/com/ruibranco/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.ruibranco.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ruibranco.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+	
+}
