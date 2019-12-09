d7d0e4fcb31bb57429044bdc5c50302987f0cfae
Rjborg <rjb.inf@gmail.com>
2019-12-09 16:14:02 +0000

    Paginação com parametros opcionais naa requesição

--- src/main/java/com/ruibranco/cursomc/services/CategoriaService.java
+++ src/main/java/com/ruibranco/cursomc/services/CategoriaService.java
@@ -2,11 +2,12 @@ package com.ruibranco.cursomc.services;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
-
 import com.ruibranco.cursomc.domain.Categoria;
 import com.ruibranco.cursomc.repositories.CategoriaRepository;
 import com.ruibranco.cursomc.services.exceptions.DataIntegrityException;
@@ -46,5 +47,10 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
